01a9f3f12202bd80b52ff99d79358f610e36ad1e
Preeti Bhardwaj <bhardwajpreeti@gmail.com>
2023-05-14 00:29:47 +0530

    assignment solution on error status code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,42 +2,51 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.js");
 
 const createUser = async function (req, res) {
-  let data = req.body;
+  try{let data = req.body;
   let savedData = await userModel.create(data);
-  res.send({ msg: savedData });
+  res.send({ msg: savedData })}
+  catch(error){
+    res.status(400).send({error:"An error occurred while creating the user's data"})
+  }
 };
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
+  try{let userName = req.body.emailId;
   let password = req.body.password;
 
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
-    return res.send({
+    return res.status(401).send({
       status: false,
       msg: "username or the password is not corerct",
     });
   let token = jwt.sign({userId: user._id.toString()},"technetium-batch");
   res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
+  res.status(201).send({ status: true, token: token });}
+  catch(error){
+  res.status(500).send({error:"user login failed"})}5
+  
 };
 
 const getUserData = async function (req, res) {
 
-  let userId = req.params.userId;
+  try{let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
+    return res.status(404).send({ status: false, msg: "No such user exists" });
 
-  res.send({ status: true, data: userDetails });
+  res.status(201).send({ status: true, data: userDetails });}
+  catch(error){
+    res.status(500).send({error:"failed to get user data"})
+  }
 };
 
 const updatedUserAttribute = async function (req, res) {
 
-  let userId = req.params.userId;
+  try{let userId = req.params.userId;
   let user = await userModel.findById(userId);
   if (!user) {
-    return res.send("No such user exists");
+    return res.status(404).send("No such user exists");
   }
 
   let userAttribute = req.body;
@@ -46,20 +55,26 @@ const updatedUserAttribute = async function (req, res) {
     userAttribute,
     {new: true}
   );
-  res.send({ status:true , data: updatedUserAttribute });
+  res.status(201).send({ status:true , data: updatedUserAttribute });}
+  catch(error){
+    res.status(500).send({error:"cannot update user detail"})
+  }
 };
 
 const updateIsDelete = async function (req, res) {
-  const userIdInfo = req.params.userId;
+  try{const userIdInfo = req.params.userId;
   let user = await userModel.findById(userIdInfo);
   //Return an error if no user with the given id exists in the db
   if (!user) {
-    return res.send("No such user exists");
+    return res.status(404).send("No such user exists");
   }
 
  if(userIdInfo){
   let updatedIsDeleted= await userModel.findOneAndUpdate({ _id: userIdInfo },{ isDeleted:true},{new:true});
-  res.send({ status: updatedIsDeleted, data: updatedIsDeleted });
+  res.status(201).send({ status: updatedIsDeleted, data: updatedIsDeleted });
+ }}
+ catch(error){
+  res.staus(500).send({error:"cannot delete user data"})
  }
 };
 
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,30 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 const middleware1 = function (req, res, next) {
-  let token = req.headers["x-auth-token"];
+  try{let token = req.headers["x-auth-token"];
   //   if (!token) token = req.headers["x-auth-token"];
-  if (!token) return res.send({ status: false, msg: "token must be present" });
+  if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
 
-  console.log(token);
+  // console.log(token);
 
-   jwt.verify(token, "technetium-batch",
-   (error,token)=>{
-    console.log(token,error)
-    if (!token)
-      return res.send({ status: false, msg: "token is invalid" });
+   jwt.verify(token, "technetium-batch")
+  next();}
 
-  });
-
-
-  next();
+  catch(error){
+    res.status(401).send({status:false , msg:"token is invalid"})
+  }
 };
 
-const middleware2= async  function(req,res,next){
+const middleware2=  function(req,res,next){
   let token = req.headers["x-auth-token"];
   let decodedToken=jwt.verify(token, "technetium-batch")
-  let userToBeModified = req.params.userId
+  let verifyUserId = req.params.userId
     let userLoggedIn =decodedToken.userId
-    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+    if( verifyUserId != userLoggedIn) return res.status(403).send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
 
     next()
 
